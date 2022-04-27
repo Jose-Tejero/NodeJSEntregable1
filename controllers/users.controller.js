2543564810e81a859abdbd24e89e23fae6554e52
Jose-Tejero <j_jtejero@hotmail.com>
2022-04-27 16:51:25 -0500

    Agregando todos los archivos necesarios para la base de datos

---
+++ controllers/users.controller.js
@@ -0,0 +1,19 @@
+const { User } = require("../models/user.model");
+
+const getAllUsers = async (req, res) => {
+  const users = await User.findAll();
+
+  res.status(200).json({
+    users,
+  });
+};
+
+const createUser = async (reque, res) => {
+  const { name, email } = req.body;
+
+  const newUser = await User.create({ newUser });
+
+  res.status(201).json({ newUser });
+};
+
+module.exports = { getAllUsers, createUser };
